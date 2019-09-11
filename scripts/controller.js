722e01f56a91acf3bd1c8a2c3ee634308b79da24
iampatik <janpatrick.tancinco@student.passerellesnumeriques.org>
2019-09-11 14:49:38 +0800

    changing some functions

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,36 +1,26 @@
-var btnConnect = document.getElementById('connect');
-var btnDisConnect = document.getElementById('disconnect');
+var btnConnect = document.getElementById('connectbtn');
+var btnDisConnect = document.getElementById('disconnectbtn');
 var btnPublish = document.getElementById('publish');
 var btnSubscribe = document.getElementById('subscribe');
 var btnUnSubscribe = document.getElementById('unsubscribe');
 var conRes = document.getElementById('connectresult');
-
-var topic;
-var payload;
 var timestamp = new Date()
 var data;
 
 // basic functionalities
- 
-alert("PLEASE CONNECT THE BROKER FIRST")
-btnConnect.addEventListener('click', function(e) {
-        e.preventDefault();
-        var brokerAdress = document.getElementById('broker').value;
-        console.log(brokerAdress);
-        client = mqtt.connect(brokerAdress);
-        client.on("connect", function(){
-          conRes.value= "Successfully connected";
-          conRes.style.backgroundColor='rgb(0, 204, 0)';
-          conRes.style.color = 'yellow';
-        console.log("Successfully connected");
-    })
-})
 
-btnPublish.addEventListener('click',function(e){
+alert("PLEASE CONNECT THE BROKER FIRST")
+btnConnect.addEventListener('click', function (e) {
       e.preventDefault();
-      payload = document.getElementById('payload').value;
-      topic = document.getElementById('topic').value
-      console.log(topic)
+      client = mqtt.connect(document.getElementById('brokerAdd').value);
+      console.log(document.getElementById('brokerAdd').value)
+      client.on("connect", function () {
+            conRes.value = "Successfully connected";
+            conRes.style.backgroundColor = 'rgb(0, 204, 0)';
+            conRes.style.color = 'yellow';
+            console.log("Successfully connected");
+
+      });
       client.on("message", function (topic, payload) {
             var table = document.getElementById('tbody');
             var row = table.insertRow(0);
@@ -41,30 +31,34 @@ btnPublish.addEventListener('click',function(e){
             cell2.innerHTML = payload;
             cell3.innerHTML = timestamp;
       })
-            client.publish(topic,payload)
-})
+});
 
-btnSubscribe.addEventListener('click',function(e) {
+btnPublish.addEventListener('click', function (e) {
       e.preventDefault();
-      topic = document.getElementById('topic').value
-      console.log(topic)
-      client.subscribe(topic)
+      // topic = document.getElementById('topic').value
 
+      client.publish(document.getElementById('topic').value, document.getElementById('payload').value)
 })
 
-btnDisConnect.addEventListener('click', function() {
-        console.log(client.end());
-        client.end();
-        conRes.value= "Successfully Disconnected";
-        conRes.style.backgroundColor='rgb(204, 0, 0)';
-            conRes.style.color = 'yellow';
+btnSubscribe.addEventListener('click', function (e) {
+      e.preventDefault();
+      client.subscribe(document.getElementById('subscribetopic').value);
+      console.log(document.getElementById('subscribetopic').value)
 })
 
-btnUnSubscribe.addEventListener('click',function(e) {
+btnDisConnect.addEventListener('click', function () {
+      console.log(client.end());
+      client.end();
+      conRes.value = "Successfully Disconnected";
+      conRes.style.backgroundColor = 'rgb(204, 0, 0)';
+      conRes.style.color = 'yellow';
+})
+
+btnUnSubscribe.addEventListener('click', function (e) {
       e.preventDefault();
       console.log('unsubscribe')
-      client.unsubscribe(topic,payload);
-  
+      client.unsubscribe(topic, payload);
+
 })
 
 
